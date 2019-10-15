07b8155e98a02b2c98b9fe7604cac7635fb9df8d
antonio gonzalez <jose_antony11@hotmail.com>
2019-10-14 22:18:25 -0700

    Initial commit

---
+++ app/src/main/java/com/example/carritodecompra/productoModelo.java
@@ -0,0 +1,12 @@
+package com.example.carritodecompra;
+
+public class productoModelo {
+
+    String Nombre;
+    int Precio;
+
+    public void productoModelo(String Nombre,int precio){
+        this.Nombre = Nombre;
+        this.Precio = precio;
+    }
+}
